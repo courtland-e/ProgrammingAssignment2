fecae3ac8479add24d7e3ae7b7d8acd4279017ad
courtland-e <t0e1006@yahoo.com>
2014-06-20 14:54:55 -0700

    Attempt 1
    Functions written based on vector cacheing example

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions (makeCacheMatrix and cacheSolve)
+## that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## Creates a matrix object that can cache inversions of itself
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## same as vector-cacheing example 
+    i <- NULL ##set to NULL until new instance is initiated
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() {x}
+    setinverse <- function(inverse) {i <<- inverse}
+    getinverse <- function() {i}
+    ## inner function values added to list
+    list(set=set, get=get,setinverse=setinverse,
+         getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the matrix object returned by
+## makeCacheMatrix, and if inverse has already been cached, merely
+## retrieves and returns the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    ## if i isn't NULL, prints message and returns cached value
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data,...)
+    x$setinverse(i)
+    i
 }
